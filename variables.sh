61c1f1c081f3207cdf25da898dcdfd03429b7d10
HP <madhankumarchavan26@gmail.com>
2024-01-04 15:13:30 +0530

    vars

--- variables.sh
+++ variables.sh
@@ -3,7 +3,7 @@ class=devops
 topic=shell script
 date=2024-1-04
 
-echo my name is $name
-echo i am preparing $class
-echo completing $topic
-echo on dat $date
+echo my name - $name
+echo i am preparing - $(class)
+echo completing - $topic
+echo on date - $date
